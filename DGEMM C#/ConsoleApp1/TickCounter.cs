170f0e2aa6808dc3190a493583d54d36e4991dd6
Aleksandr Grigoryev <AleksandrGrigoryev@itcenterifmo.onmicrosoft.com>
2023-04-17 13:53:47 +0300

    first commit

---
+++ DGEMM C#/ConsoleApp1/TickCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TickCounter
+    {
+        private DateTime end_time;
+        private DateTime start_time;
+        public TickCounter() { }
+        public void Start()
+        {
+            start_time = DateTime.Now;
+            
+        }
+        public void Stop()
+        {
+            end_time = DateTime.Now; 
+        }
+        public TimeSpan GetTime()
+
+        {
+
+            TimeSpan result = end_time - start_time;
+            return result;
+            
+        }
+    }
+}
---
+++ DGEMM java/GGEMM/src/TickCounter.java
@@ -0,0 +1,28 @@
+public class TickCounter {
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public TickCounter() {
+    }
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void end() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long result() {
+        long resultTime = endTime - startTime;
+        return resultTime;
+    }
+}
